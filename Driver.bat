49c3d07b7a6fc99f08316035d83b7173c2e02dcd
daryldcL <daryl.lomugdang@gmail.com>
2017-04-25 17:06:31 +0800

    first commit

---
+++ Driver.bat
@@ -0,0 +1,2 @@
+cd C:\Program Files\SmartBear\ReadyAPI-1.9.0\bin\
+cmd.exe /C testrunner.bat -a -j -fC:\Users\PassionITe-Daisy\TeeOff_SoapUI_Proj\reports -r C:\Users\PassionITe-Daisy\TeeOff_SoapUI_Proj
